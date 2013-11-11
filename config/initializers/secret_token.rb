b2513bf33cdedbef5f93f63310213e07da3c3499
vagrant <vagrant@ironhack>
2013-11-11 10:46:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Manderley::Application.config.secret_key_base = '7eb3d4bc7597b4c905e77a884ced9c28ed36fb90644df02808e4b846add2bf720e9138015c3222c7ae54b2f820b364a32546c7dbe7c3a4775c1a36969a1d770f'
