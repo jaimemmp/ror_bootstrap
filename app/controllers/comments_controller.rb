a4246afea6ee0ad9601befc123ad42bcea721221
Jaime Muñoz <jaimemmp@gmail.com>
2013-11-13 09:55:22 +0100

    12th november changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,59 @@
 class CommentsController < ApplicationController
-    before_action :set_comment
+    
+    before_action :set_movie, except: [:show, :edit, :update, :destroy]
 
     def index
-        @comments = Comment.all
+        @comments = @movie.comments
     end
 
     def new
-        @comment = Comment.new
+        @comment = @movie.comments.build
+    end
+
+    def show
+    end
+
+    def edit
     end
 
     def create
-    @comment = Comment.new(comment_params)
+        @comment = @movie.comments.build(comment_params)
         respond_to do |format|
           if @comment.save
-            format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+            format.html { redirect_to [@movie, @comment], notice: 'Comment was successfully created.' }
             format.json { render action: 'show', status: :created, location: @comment }
           else
             format.html { render action: 'new' }
-            format.json { render json: @comment.errors, status: :unprocessable_entity }
+            format.json { render json: [@movie, @comment].errors, status: :unprocessable_entity }
           end
         end
     end
 
     def update
+        @comment = @movie.comments.find(params[:id])
         respond_to do |format|
-          if @comment.update(comment_params)
-            format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+          if @movie.comments.update(comment_params)
+            format.html { redirect_to [@movie, @comment], notice: 'Comment was successfully updated.' }
             format.json { head :no_content }
           else
             format.html { render action: 'edit' }
-            format.json { render json: @comment.errors, status: :unprocessable_entity }
+            format.json { render json: [@movie, @comment].errors, status: :unprocessable_entity }
           end
         end
     end
 
     def destroy
+        @comment = @movie.comments.find(params[:id])
         @comment.destroy
         respond_to do |format|
-          format.html { redirect_to comments_url }
+          format.html { redirect_to comments_url(@movie, @comment) }
           format.json { head :no_content }
         end
     end
 
     private
-    def set_comment
-        @comment = Comment.new(params[:id])
+    def set_movie
+        @movie = Movie.find(params[:movie_id])
     end
 
     def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Manderley::Application.routes.draw do
-  resources :movies, except: [:destroy]
+  resources :movies, except: [:destroy] do
+    resources :comments, shallow: true
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
